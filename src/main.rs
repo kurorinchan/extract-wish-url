64be2a382a455bc47d67c53dd646c6f9f0bb47dd
kurorinchan <kurorinchan@users.noreply.github.com>
2024-10-22 22:13:08 +0900

    Find multiple urls
    - Instead of just finding the first one, find all candidates.
    - Use bstr.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use aho_corasick::AhoCorasick;
 use anyhow::{Context, Result};
+use bstr::ByteSlice;
 use enum_assoc::Assoc;
 use std::cmp::Ordering;
 use std::env;
@@ -107,7 +107,13 @@ impl PullExtractor {
         let data2_path = get_to_data2_file(&web_cache_dir).context("Failed to find data_2 file")?;
 
         let content = fs::read(data2_path).context("Failed to read data_2 file")?;
-        find_gacha_url_in_slice(&content, &self.marker, &self.url_start, &self.end_marker)
+        let urls =
+            find_gacha_urls_in_slice(&content, &self.marker, &self.url_start, &self.end_marker)?;
+        if urls.is_empty() {
+            return Err(anyhow::anyhow!("Found no gacha URLs"));
+        }
+
+        Ok(urls[0].to_string())
     }
 }
 
@@ -174,62 +180,44 @@ fn get_to_data2_file(web_cache_dir: &Path) -> Option<PathBuf> {
     Some(data2_path)
 }
 
-fn find_gacha_url_in_slice(
+fn find_gacha_urls_in_slice(
     content: &[u8],
     marker: &str,
     url_start: &str,
     end_marker: &str,
-) -> Result<String> {
-    let patterns = &[marker];
-    let ac = AhoCorasick::builder().build(patterns)?;
+) -> Result<Vec<String>> {
+    let mut urls = vec![];
+    for marker_match in content.find_iter(marker) {
+        let gacha_marker_end = marker_match + marker.len();
+        let rest_of_content = &content[gacha_marker_end..];
 
-    let gacha_marker_end = ac
-        .find(content)
-        .context(format!("Failed to find pattern {}", patterns[0]))?
-        .end();
+        // Note that this variable contains the index from the beginning of |content|.
+        let url_end_pos = rest_of_content
+            .find(end_marker)
+            .context("Failed to find end marker")?
+            + end_marker.len()
+            + gacha_marker_end;
 
-    // Keep reading until "game_biz=hk4e_global" is encountered, thats where the URL ends.
-    let rest_of_content = &content[gacha_marker_end..];
-    let patterns = &[end_marker];
-    let ac = AhoCorasick::builder().build(patterns)?;
+        // Since URLs can only be a certain length, the value in this variable is used to slice
+        // |content| to find the beginning of the URL.
+        const MAX_URL_LENGTH: usize = 2048;
+        let url_search_start_pos = url_end_pos.saturating_sub(MAX_URL_LENGTH);
 
-    let mat = ac
-        .find(rest_of_content)
-        .context(format!("Failed to find {} in file", patterns[0]))?;
+        let potential_url_slice = &content[url_search_start_pos..url_end_pos];
 
-    // Note that this variable contains the index from the beginning of |content|.
-    // Since URLs can only be a certain length, the value in this variable is used to slice
-    // |content| to find the beginning of the URL.
-    let url_end_pos = mat.end() + gacha_marker_end;
-    const MAX_URL_LENGTH: usize = 2048;
-    let url_search_start_pos = if url_end_pos < MAX_URL_LENGTH {
-        0
-    } else {
-        url_end_pos - MAX_URL_LENGTH
-    };
+        // Although there could be multiple URLs in the slice, since the slice ends
+        // with the end marker, the last occurrence of the url start marker must
+        // be the start of the URL.
+        let url_start_pos = potential_url_slice
+            .rfind(url_start)
+            .context("Failed to find url start")?;
 
-    let potential_url_slice = &content[url_search_start_pos..url_end_pos];
-
-    // There could be multiple URL start markers in the slice.
-    // So reverse the potential slice and find the reversed pattern, to find the first occurrence.
-    let mut reversed_slice: Vec<u8> = potential_url_slice.to_vec();
-    reversed_slice.reverse();
-
-    // Note that the URL start marker is also reversed here to match the reversed data above.
-    let reversed_start_url = url_start.chars().rev().collect::<String>();
-    let reversed_patterns = &[&reversed_start_url];
-    let ac = AhoCorasick::builder().build(reversed_patterns)?;
-
-    let mat = ac.find(&reversed_slice).context(format!(
-        "Failed to find reversed pattern: {}",
-        reversed_patterns[0]
-    ))?;
-
-    let target_url = &reversed_slice[..mat.end()];
-    let mut target_url: Vec<u8> = target_url.to_vec();
-    target_url.reverse();
-    let target_url = String::from_utf8(target_url.to_vec())?;
-    Ok(target_url)
+        urls.push(
+            String::from_utf8(potential_url_slice[url_start_pos..].to_vec())
+                .context("Failed to convert URL to string")?,
+        );
+    }
+    Ok(urls)
 }
 
 fn main() -> Result<()> {
@@ -261,6 +249,7 @@ mod tests {
 
     use super::*;
 
+    // Verify it can find the URL in binary data.
     #[test]
     fn find_gacha_url_only_url() {
         let test_url = "https://gs.hoyoverse.com/genshin/event/e20190909gacha-v3/index.html?anythinghere&game_biz=hk4e_global";
@@ -271,30 +260,60 @@ mod tests {
         // More irrelevant data at end.
         test_data.extend_from_slice(&[43, 100, 65, 2, 1, 4, 73]);
 
-        let result = find_gacha_url_in_slice(
+        let result = find_gacha_urls_in_slice(
             &test_data,
             "gacha-v3",
             "https://gs.hoyoverse.com/",
             "game_biz=hk4e_global",
         );
         assert!(result.is_ok());
-        assert_eq!(test_url, result.unwrap());
+        let result = result.unwrap();
+        assert_eq!(1, result.len());
+        assert_eq!(test_url, result[0]);
     }
 
-    // Verify it can find the URL in binary data.
     #[test]
     fn find_gacha_url() {
         let test_url = "https://gs.hoyoverse.com/genshin/event/e20190909gacha-v3/index.html?anythinghere&game_biz=hk4e_global";
         let test_url_vec = test_url.as_bytes().to_vec();
 
-        let result = find_gacha_url_in_slice(
+        let result = find_gacha_urls_in_slice(
             &test_url_vec,
             "gacha-v3",
             "https://gs.hoyoverse.com/",
             "game_biz=hk4e_global",
         );
         assert!(result.is_ok());
-        assert_eq!(test_url, result.unwrap());
+        let result = result.unwrap();
+        assert_eq!(1, result.len());
+        assert_eq!(test_url, result[0]);
+    }
+
+    // Verify that it can find multiple urls.
+    #[test]
+    fn find_gacha_urls_in_ascii() {
+        let test_url1 = "https://gs.hoyoverse.com/genshin/event/e20190909gacha-v3/index.html?ANYDATA11111&game_biz=hk4e_global";
+        let test_url2 = "https://gs.hoyoverse.com/genshin/event/e20190909gacha-v3/index.html?DIFFERTDATA22222&game_biz=hk4e_global";
+
+        let test_data: Vec<u8> = [
+            test_url1.as_bytes(),
+            // any data.
+            &[0xFF, 0x00, 0x3A, 0xBC],
+            test_url2.as_bytes(),
+        ]
+        .concat();
+
+        let result = find_gacha_urls_in_slice(
+            &test_data,
+            "gacha-v3",
+            "https://gs.hoyoverse.com/",
+            "game_biz=hk4e_global",
+        );
+        assert!(result.is_ok());
+        let result = result.unwrap();
+        assert_eq!(2, result.len());
+        assert_eq!(test_url1, result[0]);
+        assert_eq!(test_url2, result[1]);
     }
 
     // gacha-v3 marker is in the url but cannot find the end.
@@ -303,7 +322,7 @@ mod tests {
         let test_url =
             "https://gs.hoyoverse.com/genshin/event/e20190909gacha-v3/index.html?anythinghere";
         let test_url_vec = test_url.as_bytes().to_vec();
-        let result = find_gacha_url_in_slice(
+        let result = find_gacha_urls_in_slice(
             &test_url_vec,
             "gacha-v3",
             "https://gs.hoyoverse.com/",
@@ -317,7 +336,7 @@ mod tests {
     fn no_gacha_url_has_marker_has_end_marker_no_start_marker() {
         let test_url = "verse.com/genshin/event/e20190909gacha-v3/index.html?anythinghere";
         let test_url_vec = test_url.as_bytes().to_vec();
-        let result = find_gacha_url_in_slice(
+        let result = find_gacha_urls_in_slice(
             &test_url_vec,
             "gacha-v3",
             "https://gs.hoyoverse.com/",
